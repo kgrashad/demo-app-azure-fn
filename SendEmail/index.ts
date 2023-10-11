a61094a22e15f940e351bd9d553015cd580b9df0
Shahid Hussain <malikshahid85@hotmail.com>
2023-10-11 16:31:11 -0700

    Updated SendMail Function to beautify the message

--- SendEmail/index.ts
+++ SendEmail/index.ts
@@ -1,8 +1,11 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import * as nodemailer from "nodemailer"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as nodemailer from "nodemailer";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const { name, email, message, deviceInfo } = req.body;
+    const { name, email, subject, subjectType, message, deviceInfo } = req.body;
+
+    // Beautify JSON string
+    const deviceInfoFormatted = JSON.stringify(deviceInfo, null, 4); // Indent with 4 spaces
 
     let transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -12,11 +15,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         },
     });
 
+    // Use HTML for email content
     let mailOptions = {
         from: process.env["FromEmail"],
         to: process.env["ToEmail"],
-        subject: 'Contact Us Form Submission',
-        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nDevice Info: ${JSON.stringify(deviceInfo)}`,
+        subject: subject,
+        replyTo: email,
+        html: `
+        <p><strong>Subject Type:</strong> ${subjectType}</p>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong> ${message}</p>
+        <h2>Device Info:</h2>
+        <pre>${deviceInfoFormatted}</pre>`, // Use <pre> for preformatted text display
     };
 
     try {
